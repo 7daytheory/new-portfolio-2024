7dff3f22f42fa9ef238e61dd79f0abbf573c0a87
Matthew Lowe <matthewjlowe11@gmail.com>
2024-06-14 15:43:54 -0400

    Create Components folder, and create Navigation component

---
+++ portfolio-2024/src/components/Navigation/Navigation.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Navigation() {
+    return (
+      <>
+        <div class="logo">
+
+        </div>
+
+        <nav>
+            
+        </nav>
+      </>
+    )
+}
+
+export default Navigation;
