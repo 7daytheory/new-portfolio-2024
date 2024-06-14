326e00620bf7541e80d9f4a14b0e4e3c1a8af018
Matthew Lowe <matthewjlowe11@gmail.com>
2024-06-14 17:21:01 -0400

    Cleaned up Navigation and added styles

--- portfolio-2024/src/App.jsx
+++ portfolio-2024/src/App.jsx
@@ -1,14 +1,31 @@
-import { useState } from 'react'
-import Navigation from './components/Navigation/Navigation'
+import React from 'react';
+import Navigation from './components/Navigation/Navigation';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+    <div>
       <Navigation />
-    </>
-  )
+      {/* Other components */}
+      <div style={{ paddingTop: '70px' }}> {/* Add this style to ensure content doesn't hide behind the fixed navbar */}
+        <section id="home">
+          <h1>Home Section</h1>
+          {/* Content */}
+        </section>
+        <section id="about">
+          <h1>About Section</h1>
+          {/* Content */}
+        </section>
+        <section id="projects">
+          <h1>Projects Section</h1>
+          {/* Content */}
+        </section>
+        <section id="contact">
+          <h1>Contact Section</h1>
+          {/* Content */}
+        </section>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
