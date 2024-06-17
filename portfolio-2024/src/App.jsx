de141cf595d1527185a05bf2488d381b5c279b74
Matthew Lowe <matthewjlowe11@gmail.com>
2024-06-16 21:55:38 -0400

    Create lottie animation and added it to banner - banner needs styling

--- portfolio-2024/src/App.jsx
+++ portfolio-2024/src/App.jsx
@@ -1,30 +1,13 @@
 import React from 'react';
 import Navigation from './components/Navigation/Navigation';
+import Banner from './components/Banner/Banner';
 
 function App() {
   return (
-    <div>
-      <Navigation />
-      {/* Other components */}
-      <div style={{ paddingTop: '70px' }}> {/* Add this style to ensure content doesn't hide behind the fixed navbar */}
-        <section id="home">
-          <h1>Home Section</h1>
-          {/* Content */}
-        </section>
-        <section id="about">
-          <h1>About Section</h1>
-          {/* Content */}
-        </section>
-        <section id="projects">
-          <h1>Projects Section</h1>
-          {/* Content */}
-        </section>
-        <section id="contact">
-          <h1>Contact Section</h1>
-          {/* Content */}
-        </section>
-      </div>
-    </div>
+    <>
+     <Navigation />
+     <Banner />
+    </>
   );
 }
 
