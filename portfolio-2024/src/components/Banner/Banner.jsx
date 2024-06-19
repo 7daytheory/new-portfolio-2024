dce95563e41a26e8780fa52c4bddee1cc54a7e98
Matthew Lowe <matthewjlowe11@gmail.com>
2024-06-19 18:53:48 -0400

    Added boostrap to website structure

--- portfolio-2024/src/components/Banner/Banner.jsx
+++ portfolio-2024/src/components/Banner/Banner.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import Lottie from 'lottie-react';
 import animationData from '../../assets/lottie-header.json';
+import './Banner.css'; // Import the CSS file
 
 function Banner () {
-    return (
-    <Container fluid className="banner bg-light py-6 d-flex justify-content-between align-items-center">
-      <div className="text-container">
-        <h1 className="text-primary">Hi, I'm Matthew</h1>
-        <h2 className="text-danger">I am a Developer</h2>
-      </div>
-      <div className="animation-container">
-        <Lottie animationData={animationData} style={{ width: '70%', float: 'right'}} />
-      </div>
+  return (
+    <Container className="banner bg-light py-3 d-flex align-items-center">
+      <Row className="w-100">
+        <Col md={6} className="text-container d-flex flex-column justify-content-center align-items-start">
+          <h1 className="text-primary">Hi, I'm Matthew</h1>
+          <h2 className="text-danger">I am a Developer</h2>
+        </Col>
+        <Col md={6} className="animation-container d-flex justify-content-end">
+          <Lottie animationData={animationData} style={{ width: '100%', maxWidth: '400px', height: 'auto' }} />
+        </Col>
+      </Row>
     </Container>
   );
 };
