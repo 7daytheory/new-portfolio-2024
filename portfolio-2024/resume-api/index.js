f9cd277aa098947f3b112b884122ead0c6392cef
Matthew Lowe <matthewjlowe11@gmail.com>
2024-06-24 14:47:52 -0400

    Created resume api that will be sent in json format

--- portfolio-2024/resume-api/index.js
+++ portfolio-2024/resume-api/index.js
@@ -1,73 +1,76 @@
 const express = require('express');
+
 const app = express();
 const port = 3000;
 
-const resume = {
-    name: "Matthew Lowe",
-    title: "Software/Web Developer",
-    contact: {
-      phone: "+1 416 660 7973",
-      email: "Matthewjlowe11@gmail.com",
-      github: "https://github.com/7daytheory",
-      portfolio: "https://7daytheory.github.io/portfolio-react"
+const experiences = [
+    {
+      title: "Full Stack Developer",
+      company_name: "ShipEarly",
+      iconBg: "#ff6347",
+      date: "Aug 2023 – 2024 (9-month contract)",
+      points: [
+        "Joined a small startup on a contract to help them grow",
+        "Worked on their back end using Cake 2 PHP to develop a sales system that was integrated with salesforce",
+        "Worked with Git, did code reviews and integrated it with Trello",
+        "Communicated the need for regular meetings, which the owner would not do and caused me to leave after contract ended"
+      ],
     },
-    overview: "Over 10 years of experience working on front and back end of projects. I am well versed in Git and doing code reviews, PRs and more on GitHub. I have worked with JavaScript, PHP, MySQL, C++, SQL, APIs and more. Worked on frameworks such as React & Angular as well as Laravel & Cake. I am able to work, learn, teach and articulate ideas well with others.",
-    experience: [
-      {
-        company: "ShipEarly",
-        period: "2023 – 2024 (9-month contract)",
-        role: "Full Stack Developer",
-        responsibilities: [
-          "Joined a small startup on a contract to help them grow",
-          "Worked on their back end using Cake 2 PHP to develop a sales system that was integrated with salesforce",
-          "Worked with Git, did code reviews and integrated it with Trello",
-          "Communicated the need for regular meetings, which were not implemented"
-        ]
-      },
-      {
-        company: "Quillsoft",
-        period: "2017 - 2023",
-        role: "Developer",
-        responsibilities: [
-          "Created and oversaw growth of the CRM system",
-          "Built a PWA of the main desktop application for schools",
-          "Developed a license management system",
-          "Implemented up-to-date encryption methods"
-        ]
-      },
-      {
-        company: "Strategic Transitions Inc",
-        period: "2014 – 2017",
-        role: "Developer",
-        responsibilities: [
-          "Created and integrated a license management system",
-          "Developed encryption for user data security",
-          "Developed a customized CMS system",
-          "Wireframed, designed, and coded company websites"
-        ]
-      },
-      {
-        company: "NeXsys",
-        period: "2013",
-        role: "BPMS Administrator",
-        responsibilities: [
-          "Created and integrated a license management system",
-          "Developed encryption for user data security",
-          "Developed a customized CMS system",
-          "Wireframed, designed, and coded company websites"
-        ]
-      }
-    ],
-    skills: [
-      "Outgoing", "Self-Driven", "Accountable", "Independent", "Proactively identify new tasks", "Collaborating with colleagues", "Always learning"
-    ],
-    references: "Available Upon Request"
-  };
+    {
+      title: "Front End Developer",
+      company_name: "Lawlabs",
+      iconBg: "#ff6347",
+      date: "2021 - 2023",
+      points: [
+        "Worked on their SASS product using Angular",
+        "Worked as a team to turn designs into functioning website or SASS functionality",
+        "Worked with Git, did code reviews and stand ups every morning",
+        "Presented work and ideas to the owner regularly"
+      ],
+    },
+    {
+      title: "Developer",
+      company_name: "Quillsoft",
+      iconBg: "#46b1c9",
+      date: "2017 - 2021",
+      points: [
+        "Created and oversaw growth of the CRM system",
+        "Built a PWA of the main desktop application for schools",
+        "Developed a license management system",
+        "Implemented up-to-date encryption methods"
+      ],
+    },
+    {
+      title: "Developer",
+      company_name: "Strategic Transitions Inc",
+      iconBg: "#9ccd5d",
+      date: "2014 – 2017",
+      points: [
+        "Created and integrated a license management system",
+        "Developed encryption for user data security",
+        "Developed a customized CMS system",
+        "Wireframed, designed, and coded company websites"
+      ],
+    },
+    {
+      title: "BPMS Administrator",
+      company_name: "NeXsys",
+      iconBg: "#5eb54a",
+      date: "2013",
+      points: [
+        "Created and integrated a license management system",
+        "Developed encryption for user data security",
+        "Developed a customized CMS system",
+        "Wireframed, designed, and coded company websites"
+      ],
+    },
+  ];
+  
 
-app.get('/resume', (req, res) => {
-  res.json(resume);
-});
-
-app.listen(port, () => {
-  console.log(`Resume API listening at http://localhost:${port}`);
-});
+  app.get('/resume', (req, res) => {
+    res.json({ experiences });
+  });
+  
+  app.listen(port, () => {
+    console.log(`Resume API listening at http://localhost:${port}`);
+  });
