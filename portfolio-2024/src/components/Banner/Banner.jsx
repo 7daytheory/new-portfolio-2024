949ea45431da20b3d736125b5f492a3a122a849a
Matthew Lowe <matthewjlowe11@gmail.com>
2024-06-24 07:40:52 -0400

    Added bootstrap to all components

--- portfolio-2024/src/components/Banner/Banner.jsx
+++ portfolio-2024/src/components/Banner/Banner.jsx
@@ -2,22 +2,23 @@ import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import Lottie from 'lottie-react';
 import animationData from '../../assets/lottie-header.json';
-import './Banner.css'; // Import the CSS file
+import './Banner.css';
 
-function Banner () {
+function Banner() {
   return (
-    <Container className="banner bg-light py-3 d-flex align-items-center">
-      <Row className="w-100">
-        <Col md={6} className="text-container d-flex flex-column justify-content-center align-items-start">
-          <h1 className="text-primary">Hi, I'm Matthew</h1>
-          <h2 className="text-danger">I am a Developer</h2>
+    <Container fluid className="banner bg-light py-5 d-flex align-items-center justify-content-center">
+      <Row className="w-100 text-center">
+        <Col md={6} className="text-container d-flex flex-column justify-content-center align-items-center">
+          <h1 className="text-primary">Hello.</h1>
+          <h2 className="text-danger">I am Matthew Lowe</h2>
+          <h3 className="text-success">& I am a Developer</h3>
         </Col>
-        <Col md={6} className="animation-container d-flex justify-content-end">
+        <Col md={6} className="animation-container d-flex justify-content-center">
           <Lottie animationData={animationData} style={{ width: '100%', maxWidth: '400px', height: 'auto' }} />
         </Col>
       </Row>
     </Container>
   );
-};
+}
 
 export default Banner;
