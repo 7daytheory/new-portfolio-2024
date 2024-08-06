f3bc2351f58c5d99856728b688c734dd945a4248
Matthew Lowe <matthewjlowe11@gmail.com>
2024-08-06 13:58:13 -0400

    Update vite config file

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
-
-module.exports = {
+  css: {
+    preprocessorOptions: {
+      css: {
+        additionalData: `
+          @tailwind base;
+          @tailwind components;
+          @tailwind utilities;
+        `,
+      },
+    },
+  },
   theme: {
     extend: {
       keyframes: {
@@ -27,4 +34,4 @@ module.exports = {
   },
   variants: {},
   plugins: [],
-};
+});
