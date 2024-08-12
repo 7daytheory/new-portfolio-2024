8756cf7ef217e670813534e17aa2b395da646736
Matthew Lowe <matthewjlowe11@gmail.com>
2024-08-12 13:14:09 -0400

    Added Banner/Hero with a lottie animation

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,11 +1,29 @@
 import React from "react";
-import "./Banner.css";
+import { Fade } from "react-reveal";
+import lottieHeader from "../../assets/lottie/lottieHeader.json";
+import DisplayLottie from "../../components/DisplayLottie";
 
 function Banner() {
   return (
-    <>
-    Banner
-    </>
+    <Fade bottom duration={1000} distance="40px">
+      <div className="greet-main mx-auto mt-8 w-[90%] p-5" id="banner">
+        <div className="banner-main flex">
+          <div className="banner-text-div flex-1 mb-8">
+            <div>
+              <h1 className="banner-text text-[70px] leading-[1.1] text-black">
+                Hi. My name is Matthew.
+              </h1>
+              <p className="banner-text-p text-[30px] leading-10 text-gray-600">
+                I'm a software developer.
+              </p>
+            </div>
+          </div>
+          <div className="banner-image-div flex-1">
+              <DisplayLottie animationData={lottieHeader} />
+          </div>
+        </div>
+      </div>
+    </Fade>
   );
 }
 
