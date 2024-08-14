a71344436031ea81a905cb64ac2ee46651b723c6
Matthew Lowe <matthewjlowe11@gmail.com>
2024-08-14 13:26:46 -0400

    Added banner content with some basic styling

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -9,7 +9,7 @@ const Banner = () => {
             <div>
               <h1 className="banner-text">Hello, I'm Matthew!</h1>
               <p className="banner-text-p subTitle">
-                I'm a developer who has experience building Web and Mobile applications with JavaScript , React , PHP, Bootstrap, HTML/CSS(obviously) and other cool libraries and frameworks.
+                I'm a developer with 10 years experience in building Web and Mobile applications with JavaScript , React/Angular, PHP, Laravel/CakePHP, Bootstrap, Flexbox and other cool libraries and frameworks.
               </p>
             </div>
           </div>
