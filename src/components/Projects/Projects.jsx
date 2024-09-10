1937ed91838f07e1edbb2d0a57eb9a45eae3da69
Matthew Lowe <matthewjlowe11@gmail.com>
2024-09-10 08:33:18 -0400

    Updated emailjs template, and fixed contact FORM_KEY value

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -9,10 +9,10 @@ const Projects = () => {
   return (
     <>
         <div id="projects" className="relative w-full p-4 text-center bg-white sm:p-8 dark:bg-white dark:border-red-700">
-        <Fade duration={1000}>
+        <Fade duration={1000} triggeOnce> 
             <div className="text-red-800 text-[3em] absolute ml-[2%] top-[-23px] font-bold">SOME PROJECTS <FontAwesomeIcon icon={faArrowDown} /></div>
             <div className="flex justify-center space-x-4 mt-12">
-            <Fade duration={750} direction='right' cascade triggerOnce>
+            <Fade duration={500} direction='right' cascade triggerOnce>
               <ProjectCard />
               <ProjectCard />
               <ProjectCard />
