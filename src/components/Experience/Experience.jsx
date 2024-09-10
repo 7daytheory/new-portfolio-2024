03b0cb641bb3460acc568ebe79fef814181c4b0c
Matthew Lowe <matthewjlowe11@gmail.com>
2024-09-10 16:36:07 -0400

    Fixes #126 Change overall layout (#127)
    * Changed bg and text color of most components
    
    * Moved close btn in the new form design
    
    * Removed Request a quote from navigation

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -54,7 +54,7 @@ const Experience = () => {
     <>
       <div className='flex flex-col bg-slate-800 mt-[50px]'>
         <VerticalTimeline>
-        <Fade duration={3500} triggerOnce><h1 className="text-white text-[3em] absolute ml-[10%] top-[-22px] font-bold" id="experience">EXPERIENCE <FontAwesomeIcon icon={faArrowDown} /></h1></Fade>
+        <Fade duration={2500}><h1 className="text-white text-[3em] absolute ml-[5%] top-[-22px] font-bold" id="experience">EXPERIENCE <FontAwesomeIcon icon={faArrowDown} /></h1></Fade>
           {experiences.map((experience, index) => (
             <ExperienceCard
               key={`experience-${index}`}
