acca882cfe7eb1d541d529643b2695b14519fe4e
Matthew Lowe <matthewjlowe11@gmail.com>
2024-12-09 15:03:19 -0500

    Feat: Entire project card is clickable, updated sizes and widrh

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -1,24 +1,36 @@
-import "./ProjectCard.css"
+import "./ProjectCard.css";
 
 const ProjectCard = ({ cardInfo }) => {
+  const { projectName, projectDesc, projectImage, link } = cardInfo;
 
-  //Can do this right inside params declaration also but less flexible and less ability for error handling if it get's more complex
-  const {projectName, projectDesc, projectImage, link} = cardInfo;
-  
   return (
-    <div className="inline-block relative sm:w-full sm:max-w-[300px] lg:max-w-[500px] lg:min-w-[300px] min-w-0 h-[400px] bg-slate-800 rounded-[30px] overflow-hidden border border-2 border-white shadow-lg m-8 hover:shadow-lg">
-          <img className="hero-profile-img h-[70%] w-full" src={projectImage} alt={projectName} />
-        <div className="hero-description-bk"></div>
-        <div className="h-12 w-auto rounded-[20px] absolute bottom-[30%] left-8 font-extrabold text-white">
-          <p>{projectName}</p>
-        </div>
-      <div className="absolute bottom-[20%] left-8 text-white lg:max-w-[50%] sm:max-w-[80%] mr-[10%] sm:mx-auto text-left">
-        <p className="text-sm">{projectDesc}</p>
+    <a
+      href={link}
+      className="relative sm:w-full sm:max-w-[300px] lg:max-w-[300px] lg:min-w-[300px] h-[300px] lg:h-[300px] rounded-[30px] overflow-hidden bg-slate-800 border border-2 border-white shadow-lg m-8 hover:shadow-xl flex flex-col group"
+      title={`View details about ${projectName}`}
+    >
+      {/* Image Section */}
+      <div className="h-[60%] w-full bg-slate-800">
+        <img
+          className="h-full w-full object-cover transition-transform group-hover:scale-105"
+          src={projectImage}
+          alt={projectName}
+        />
       </div>
-        <div className="absolute right-8 bottom-[5%] py-2 px-4 bg-red-800 hover:bg-red-700 rounded-md pointer">
-            <a href={link} className="text-white font-bold px-4 py-2 lg:my-2 sm:my-8">View Project</a>
+
+      {/* Content Section */}
+      <div className="flex-1 flex flex-col justify-between p-4">
+        {/* Project Name */}
+        <div>
+          <p className="font-extrabold text-white text-lg">{projectName}</p>
         </div>
-    </div>
+
+        {/* Project Description */}
+        <div>
+          <p className="text-sm text-gray-300">{projectDesc}</p>
+        </div>
+      </div>
+    </a>
   );
 };
 
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -10,11 +10,11 @@ const Projects = () => {
   return (
       <div id="projects" className="relative w-full p-4 text-center bg-red-800 sm:p-8">
           <Fade duration={2000}> 
-            <div className="text-white text-[3em] absolute ml-[5%] top-[-23px] font-bold">PROJECTS <FontAwesomeIcon icon={faArrowDown} /></div>
+            <div className="text-white text-[3em] absolute sm:ml-[5%] lg:ml-[15%] top-[-23px] font-bold">PROJECTS <FontAwesomeIcon icon={faArrowDown} /></div>
           </Fade>
             <div className="flex justify-center space-x-4 mt-12">
               <Fade duration={500} triggerOnce cascade >
-              <div className="flex flex-wrap justify-between gap-6">
+              <div className="flex flex-wrap justify-between gap-6 w-[70%] w-[70%] mx-auto">
                 {allProjects.projects.map((card, i) => {
                       return (
                         <ProjectCard
