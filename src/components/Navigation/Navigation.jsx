344673ed66399647ca1f0f899d6243f095517ddc
Matthew Lowe <matthewjlowe11@gmail.com>
2024-12-09 15:05:06 -0500

    Feat: Clean up mobile layout, sizing, overflow

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -10,29 +10,29 @@ const Navigation = () => {
     };
 
     const navMenu = (
-        <ul className="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
+        <ul className="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent bg-gray-800 md:bg-transparent border-gray-700">
         <li>
-            <a href="#" className="block py-2 px-3 md:p-0 text-white bg-red-700 rounded md:bg-transparent md:text-700 md:dark:text-red-500 dark:bg-red-600 md:dark:bg-transparent" aria-current="page">
+            <a href="#" className="block py-2 px-3 md:p-0 text-white bg-red-700 rounded md:bg-transparent md:text-700 md:text-red-500 bg-red-600 md:bg-transparent" aria-current="page">
                 Home
             </a>
         </li>
         <li>
-            <a href="#experience" className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-red-700 dark:text-white md:dark:hover:text-red-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+            <a href="#experience" className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-red-700 text-white md:hover:text-red-500 hover:bg-gray-700 hover:text-white md:hover:bg-transparent">
                 Experience
             </a>
         </li>
         <li>
-            <a href="#resume" className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-red-700 dark:text-white md:dark:hover:text-red-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+            <a href="#resume" className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-red-700 text-white md:hover:text-red-500 hover:bg-gray-700 hover:text-white md:hover:bg-transparent">
                 Resume
             </a>
         </li>
         <li>
-            <a href="#projects" className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-red-700 dark:text-white md:dark:hover:text-red-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+            <a href="#projects" className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-red-700 text-white md:hover:text-red-500 hover:bg-gray-700 hover:text-white md:hover:bg-transparent">
                 Examples
             </a>
         </li>
         <li>
-            <a href="#contact" className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-red-700 dark:text-white md:dark:hover:text-red-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+            <a href="#contact" className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-red-700 text-white md:hover:text-red-500 hover:bg-gray-700 hover:text-white md:hover:bg-transparent">
                 Contact
             </a>
         </li>
@@ -51,7 +51,7 @@ const Navigation = () => {
                         onClick={handleToggle}
                         data-collapse-toggle="navbar-solid-bg"
                         type="button"
-                        className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                        className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 text-gray-400 hover:bg-gray-700 focus:ring-gray-600"
                         aria-controls="navbar-solid-bg"
                         aria-expanded={mobileMenu}>
                         <span className="sr-only">Open main menu</span>
