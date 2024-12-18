5a35caf880c9e5a860b9bbd36393837ef34c6005
Matthew Lowe <matthewjlowe11@gmail.com>
2024-12-18 14:22:00 -0500

    Feat: Updated styling for the label addition and cards

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -4,19 +4,19 @@ const ProjectCard = ({ cardInfo }) => {
   return (
     <a
       href={link}
-      className="relative lg:w-[300px] min-w-[250px] lg:min-w-[300px] sm:w-[90%] h-[300px] sm:h-[250px] rounded-lg overflow-hidden bg-slate-800 border border-2 border-white m-8 hover:shadow-xl flex flex-col group"
+      className="relative lg:w-[300px] min-w-[250px] lg:min-w-[300px] sm:w-[90%] h-[300px] hover:duration-700 hover:opacity-[0.9] sm:h-[250px] rounded-lg overflow-hidden bg-slate-800 border border-2 border-white shadow-[rgba(0,0,15,0.7)_2px_3px_3px_0px] hover:shadow-[rgba(0,0,15,1)_2px_5px_5px_1px] m-8 flex flex-col group"
       title={`View details about ${projectName}`}
     >
       {
        projectLabel && (
-        <div className="absolute top-[-3px] right-[-2px] bg-white text-red-800 px-2 py-1 rounded-l-md border-white border border-3 font-bold text-md">
-          <span className="mr-2">{projectLabel} only</span>
+        <div className="absolute top-[-3px] right-0 bg-white text-slate-800 px-2 py-1 rounded-l-md font-semibold text-sm">
+          {projectLabel} only
         </div>
        )
       }
       <div className="h-[60%] w-full bg-slate-800">
         <img
-          className="h-full w-full object-cover transition-transform group-hover:scale-105"
+          className="h-full w-full object-cover"
           src={projectImage}
           alt={projectName}
         />
